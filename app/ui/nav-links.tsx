2e66ae3073a392bb4b7c1340dab4bee3a666583a
maximotodev <maximotodev@proton.me>
2025-02-18 12:29:19 -0500

    Change font and color to nav-links

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 
 // Define an array of navigation links with their respective paths and labels
 const navLinks = [
-  { href: "/", label: "home" },
-  { href: "/videos", label: "videos" },
-  { href: "/music", label: "music" },
-  { href: "/events", label: "events" },
-  { href: "/store", label: "store" },
+  { href: "/", label: "Home" },
+  { href: "/videos", label: "Videos" },
+  { href: "/music", label: "Music" },
+  { href: "/events", label: "Events" },
+  { href: "/store", label: "Store" },
 ];
 
 export function Links() {
@@ -17,14 +17,14 @@ export function Links() {
   const pathname = usePathname();
 
   return (
-    <nav className="flex flex-row items-center justify-between bg-dark-100 p-4">
+    <nav className="flex flex-row items-center justify-center bg-dark-100 p-4 w-full text-center">
       {/* Map through the navLinks array to generate navigation links dynamically */}
       {navLinks.map(({ href, label }) => (
         <Link
           key={href} // Unique key for each link
           href={href} // Navigation path
-          className={`mr-4 text-white-700 hover:text-blue-500 ${
-            pathname === href ? "text-blue-500 font-bold" : ""
+          className={`mx-4 my-2 md:my-0 text-white-700 hover:text-pink-300 uppercase ${
+            pathname === href ? "text-pink-400 font-bold" : ""
           }`} // Apply active styling if the pathname matches the link
         >
           {label} {/* Display the label for each link */}
