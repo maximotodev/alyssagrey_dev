c89bb9bcecfb50ee469e49ea5800abae36c38122
maximotodev <maximotodev@proton.me>
2025-02-19 15:15:12 -0500

    Fix slides responsiveness to smaller screens

--- app/components/TracksCarousel.tsx
+++ app/components/TracksCarousel.tsx
@@ -4,7 +4,7 @@ import React, { useRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Track } from "@/lib/types";
-import { Swiper, SwiperSlide, SwiperRef } from "swiper/react"; // Import SwiperRef
+import { Swiper, SwiperSlide, SwiperRef } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -16,12 +16,11 @@ interface TracksCarouselProps {
 }
 
 export default function TracksCarousel({ tracks }: TracksCarouselProps) {
-  const swiperRef = useRef<SwiperRef>(null); // Use SwiperRef type
+  const swiperRef = useRef<SwiperRef>(null);
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handlePlay = () => {
     if (swiperRef.current && swiperRef.current.swiper) {
-      // Access swiper instance
       const currentTrack = tracks[activeIndex];
       if (currentTrack) {
         window.open(currentTrack.songUrl, "_blank");
@@ -35,11 +34,11 @@ export default function TracksCarousel({ tracks }: TracksCarouselProps) {
         slidesPerView={3}
         centeredSlides={true}
         loop={true}
-        spaceBetween={-25}
+        spaceBetween={0}
         pagination={{ clickable: true }}
         modules={[Navigation, Pagination]}
         className="w-full max-w-6xl"
-        onSwiper={(swiper) => (swiperRef.current = { swiper })} // Assign object with swiper property
+        onSwiper={(swiper) => (swiperRef.current = { swiper })}
         onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
       >
         {tracks.map((track, index) => (
@@ -47,30 +46,29 @@ export default function TracksCarousel({ tracks }: TracksCarouselProps) {
             key={track.songUrl}
             className={`flex justify-center transition-transform duration-500 relative ${
               index === activeIndex
-                ? "scale-150 z-30 opacity-100 w-[700px] h-[700px]" // Increased size for active slide
-                : "scale-50 z-10 opacity-30 w-[300px] h-[300px]" // Smaller size for other slides
+                ? "scale-150 z-30"
+                : "scale-75 z-10 opacity-30"
             }`}
           >
             <Link className="flex justify-center" href={track.songUrl}>
               <div className="text-center relative">
-                <h1
-                  className={`text-white font-bold mb-2 text-sm sm:text-md md:text-lg lg:text-xl relative z-40 transition-opacity duration-500 ${
-                    index === activeIndex
-                      ? "opacity-100 block"
-                      : "opacity-0 hidden" // Added classes here
-                  }`}
-                >
-                  {track.title}
-                </h1>
                 <Image
                   priority
                   src={track.albumArtUrl}
                   alt={track.title}
-                  width={index === activeIndex ? 500 : 300} // Adjust for larger screens
-                  height={index === activeIndex ? 500 : 300} // Adjust for larger screens
-                  // Responsive image sizing
-                  className="w-full h-full md:w-auto md:h-auto rounded-lg shadow-2xl"
+                  width={index === activeIndex ? 500 : 250}
+                  height={index === activeIndex ? 500 : 250}
+                  className="rounded-lg shadow-2xl transition-all duration-500"
                 />
+                <h1
+                  className={`text-white m-5 font-bold text-sm sm:text-md md:text-lg lg:text-xl relative z-40 transition-opacity duration-500 whitespace-nowrap ${
+                    index === activeIndex
+                      ? "opacity-100 block"
+                      : "opacity-0 hidden"
+                  }`}
+                >
+                  {track.title}
+                </h1>
               </div>
             </Link>
           </SwiperSlide>
@@ -78,7 +76,7 @@ export default function TracksCarousel({ tracks }: TracksCarouselProps) {
       </Swiper>
       <div className="flex justify-center gap-4 mt-4">
         <button
-          onClick={() => swiperRef.current?.swiper.slidePrev()} // Access swiper property
+          onClick={() => swiperRef.current?.swiper.slidePrev()}
           className="bg-gray-800 text-white p-2 rounded-full"
         >
           <FaStepBackward size={24} />
@@ -90,7 +88,7 @@ export default function TracksCarousel({ tracks }: TracksCarouselProps) {
           <FaPlay size={24} />
         </button>
         <button
-          onClick={() => swiperRef.current?.swiper.slideNext()} // Access swiper property
+          onClick={() => swiperRef.current?.swiper.slideNext()}
           className="bg-gray-800 text-white p-2 rounded-full"
         >
           <FaStepForward size={24} />
